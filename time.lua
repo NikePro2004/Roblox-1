9976dc3ba590f7d986e3846b17422f0b6197d15b
NikePro2004 <edgarsjanis@gmail.com>
2015-10-30 18:15:58 +0200

    Update time.lua

--- time.lua
+++ time.lua
@@ -1,27 +1,13 @@
-GetTime=function(_time)
-	local _day,_hour,_min,_sec;
-	_day=math.floor(_time/86400);
-	_time=_time-_day*86400;
-	_hour=math.floor(_time/3600);
-	_time=_time-_hour*3600;
-	_min=math.floor(_time/60);
-	_time=_time-_min*60;
-	_sec=math.floor(_time/1);
-	local __time='';
-	if(tostring(_hour):len()==1)then
-		__time=__time..'0'.._hour..':';
-	else
-		__time=__time.._hour..':';
-	end;
-	if(tostring(_min):len()==1)then
-		__time=__time..'0'.._min..':';
-	else
-		__time=__time.._min..':';
-	end;
-	if(tostring(_sec):len()==1)then
-		__time=__time..'0'.._sec;
-	else
-		__time=__time.._sec;
-	end;
-	return(__time);
-end;
+lastSpeed=1;
+
+function checkKeys(timer)
+if (isKeyPressed(VK_E)) then if lastspeed ~= 10 then speedhack_setSpeed(10) lastSpeed=10 end
+else if lastspeed ~= 1 then speedhack_setSpeed(1) lastSpeed=1 end
+end
+
+end
+
+t=createTimer(nil)
+timer_setInterval(t, 100)
+timer_onTimer(t, checkKeys)
+timer_setEnabled(t, true)
